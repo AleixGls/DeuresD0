7b96ca416763d9dcf000bfffca0471ba66e61603
AleixGls <aleixgls@gmail.com>
2024-11-13 19:51:26 +0100

    exercicis010-011 completats

--- Exercicis/exercici010.py
+++ Exercicis/exercici010.py
@@ -8,12 +8,25 @@ import sys
 #---|COLORS|-----------------------------------------------------------------------------------------
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-GRAY = (200, 200, 200)
-PINK = (255, 105, 180)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE  = (0, 0, 255)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0) 
+GOLD = (255, 215, 0)
+NAVY = (0, 0, 128)
 
+mouse_pos = {"x":-1,"y":-1}
+
+rectangles = [
+    { "rect": { "x": 50, "y": 100, "width": 250, "height": 50 }, "color": RED },
+    { "rect": { "x": 50, "y": 200, "width": 100, "height": 200 }, "color": GOLD },
+    { "rect": { "x": 200, "y": 200, "width": 100, "height": 100 }, "color": BLUE },
+    { "rect": { "x": 200, "y": 350, "width": 400, "height": 50 }, "color": PURPLE },
+    { "rect": { "x": 350, "y": 100, "width": 50, "height": 200 }, "color": ORANGE },
+    { "rect": { "x": 450, "y": 100, "width": 150, "height": 100 }, "color": GREEN },
+    { "rect": { "x": 450, "y": 250, "width": 150, "height": 50 }, "color": NAVY }
+]
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -39,19 +52,61 @@ def main():
 
 #---|EVENTS|-----------------------------------------------------------------------------------------
 def app_events():
+    global mouse_pos
+    mouse_inside = pygame.mouse.get_focused() # El ratolí està dins de la finestra?
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # Botó tancar finestra
             return False
+        elif event.type == pygame.MOUSEMOTION:
+            if mouse_inside:
+                mouse_pos["x"] = event.pos[0]
+                mouse_pos["y"] = event.pos[1]
+            else:
+                mouse_pos["x"] = -1
+                mouse_pos["y"] = -1
     return True
 
 #---|CALCULS|----------------------------------------------------------------------------------------
 def app_run():
-    pass
+    global mouse_pos, rectangle
+
+    rectangle = -1
+    for q in range (0,len(rectangles)):
+        if utils.is_point_in_rect(mouse_pos,rectangles[q]["rect"]):
+            rectangle = q
+        
 
 #---|DIBUIX|-----------------------------------------------------------------------------------------
 def app_draw():
+    global rectangle
+
     screen.fill(WHITE)
     utils.draw_grid(pygame, screen, 50)
+    
+    for q in range (0,len(rectangles)):
+        if rectangle == q:
+            pygame.draw.rect(
+                screen,
+                rectangles[q]["color"],
+                (
+                    rectangles[q]["rect"]["x"],
+                    rectangles[q]["rect"]["y"],
+                    rectangles[q]["rect"]["width"],
+                    rectangles[q]["rect"]["height"]
+                )
+            )
+        pygame.draw.rect(
+            screen,
+            BLACK,
+            (
+                rectangles[q]["rect"]["x"],
+                rectangles[q]["rect"]["y"],
+                rectangles[q]["rect"]["width"],
+                rectangles[q]["rect"]["height"]
+            ),
+            5
+        )
 
 
     pygame.display.update()
--- Exercicis/exercici011.py
+++ Exercicis/exercici011.py
@@ -14,9 +14,13 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 
-
 pygame.init()
 clock = pygame.time.Clock()
+
+direccio = "none"
+
+pos_x = 100
+size = 25
 
 #---|FINESTRA|---------------------------------------------------------------------------------------
 screen = pygame.display.set_mode((640, 480))
@@ -39,20 +43,58 @@ def main():
 
 #---|EVENTS|-----------------------------------------------------------------------------------------
 def app_events():
+    global direccio
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # Botó tancar finestra
             return False
+        elif event.type == pygame.KEYDOWN:  # Tecla premuda
+            if event.key == pygame.K_LEFT:
+                direccio = 'left'
+            elif event.key == pygame.K_RIGHT:
+                direccio = 'right'
+        elif event.type == pygame.KEYUP:  # Tecla alliberada
+            if event.key == pygame.K_LEFT:
+                if direccio == 'left':
+                    direccio = 'none'
+            elif event.key == pygame.K_RIGHT:
+                if direccio == 'right':
+                    direccio = 'none'
     return True
 
 #---|CALCULS|----------------------------------------------------------------------------------------
 def app_run():
-    pass
+    global pos_x, size
+
+    delta_time = clock.get_time() / 1000.0  # Convertir a segons
+    velocitat = 100
+    des = velocitat * delta_time
+
+    limit_r = screen.get_width()
+
+    if direccio == "left":
+        if pos_x - size <= 0:
+            direccio == "none"
+        else:
+            pos_x -= des
+            size = 10 + (pos_x / 8)
+    elif direccio == "right":
+        if pos_x + size >= limit_r:
+            direccio == "none"
+        else:
+            pos_x += des
+            size = 10 + (pos_x / 8) 
 
 #---|DIBUIX|-----------------------------------------------------------------------------------------
 def app_draw():
     screen.fill(WHITE)
     utils.draw_grid(pygame, screen, 50)
 
+    fontArial = pygame.font.SysFont("Arial", 23)
+    text = fontArial.render("Apreta les tecles (left/right)", True, BLACK)
+    screen.blit(text, (50,50))
+
+    pygame.draw.circle(screen, BLACK, (pos_x,250), size)
 
     pygame.display.update()
 
